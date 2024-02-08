7684d2817366d0b22d55d1b14ad8b508d401683c
Lahoucine Ijioui <ije1work@gmail.com>
2024-02-08 15:00:07 +0100

    login controller

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,9 +2,14 @@ import express from "express";
 const router = express.Router();
 import authControllers from "../controllers/auth.controllers.js";
 import validateUserCreationInput from "../middlewares/validateUserCreationInput.js";
+import validateUserLoginInput from "../middlewares/validateUserLogin.js";
+import checkDuplicatedCredentials from "../middlewares/checkDuplicatedCredenials.js";
 
-// POST @/api/users/signup
-router.post('/signup', validateUserCreationInput, authControllers.signup);
+// POST create user  @/api/users/signup
+router.post('/signup', validateUserCreationInput, checkDuplicatedCredentials, authControllers.signup);
+
+// POST user login @/api/users/login
+router.post('/login', validateUserLoginInput, authControllers.login);
 
 
 export default router;
