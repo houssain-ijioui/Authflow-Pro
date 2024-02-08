995c23601afda78affa3b7ed74af97ef8b6c9824
Lahoucine Ijioui <ije1work@gmail.com>
2024-02-08 14:51:16 +0100

    check if user credentials already exist with middleware

---
+++ backend/middlewares/checkDuplicatedCredenials.js
@@ -0,0 +1,27 @@
+import User from "../models/user.model.js";
+
+
+
+const checkDuplicatedCredentials = async (req, res, next) => {
+    try {
+        const { email } = req.body;
+
+        const checkEmail = await User.find({ email });
+
+        if (checkEmail) {
+            return res.status(400).send({
+                message: "Failed! Email Already in use."
+            })
+        }
+
+        next()
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            message: "Oops somehing went wrong"
+        })
+    }
+}
+
+
+export default checkDuplicatedCredentials;
